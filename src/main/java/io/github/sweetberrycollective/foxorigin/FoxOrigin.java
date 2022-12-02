600cbc21a7b8f0f8462b9beece89e80667c7625a
Octal <59418903+oliver-makes-code@users.noreply.github.com>
2022-12-01 19:24:02 -0600

    Set up repo

---
+++ src/main/java/io/github/sweetberrycollective/foxorigin/FoxOrigin.java
@@ -0,0 +1,15 @@
+package io.github.sweetberrycollective.foxorigin;
+
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FoxOrigin implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("Example Mod");
+
+	@Override
+	public void onInitialize(ModContainer mod) {
+
+	}
+}
